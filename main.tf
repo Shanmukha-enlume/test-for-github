bb33a07a1d3a22c8f4094aa689a7634be257affa
Shanmukha-enlume <95464411+Shanmukha-enlume@users.noreply.github.com>
2021-12-14 17:09:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_storage_bucket" "bucket" {
   location = "EU"
 }
 
-resource "google_storage_bucket" "bucket" {
+resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-019123"
   location = "EU"
 }
