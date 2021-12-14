81da358c0b25a9d85e4510770a31dc028a08a17a
Shanmukha-enlume <95464411+Shanmukha-enlume@users.noreply.github.com>
2021-12-14 16:50:00 +0530

    storage bucket given location

--- main.tf
+++ main.tf
@@ -1,7 +1,36 @@
-resource "google_storage_bucket" "bucket" {
-  name = "test-bucket-random-091122"
-}
+resource "google_storage_bucket" "static-site" {
+  name          = "test-bucket-random-091122"
+  location      = "EU"
+  force_destroy = true
 
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-091123"
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
+resource "google_storage_bucket" "static-site" {
+  name          = "test-bucket-random-091123"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
 }
